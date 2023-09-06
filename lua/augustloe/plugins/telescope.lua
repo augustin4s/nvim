68bccba3b3e8f7ed3a81d3594ecf589a46508bba
Augustinas <augustinas.p@8devices.com>
2023-09-06 10:10:04 +0300

    telescope: enable hidden files search

--- lua/augustloe/plugins/telescope.lua
+++ lua/augustloe/plugins/telescope.lua
@@ -17,6 +17,25 @@ telescope.setup({
                                 ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist
                         }
                 }
+        },
+        pickers = {
+                -- Default configuration for builtin pickers goes here:
+                -- picker_name = {
+                --   picker_config_key = value,
+                --   ...
+                -- }
+                -- Now the picker_config_key will be applied every time you call this
+                -- builtin picker
+                find_files = {
+                        hidden = true
+                }
+        },
+        extensions = {
+                -- Your extension configuration goes here:
+                -- extension_name = {
+                --   extension_config_key = value,
+                -- }
+                -- please take a look at the readme of the extension you want to configure
         }
 })
 
